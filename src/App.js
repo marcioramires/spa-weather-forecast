440a0a91cc297187a60b8cefe8f908051cd42bad
marcioramires <marcioramiresgt@gmail.com>
2022-11-24 02:04:25 -0300

    API connected on screen

--- src/App.js
+++ src/App.js
@@ -4,43 +4,92 @@ import axios from "axios"
 
 function App() {
   const [weatherForecast, setWeatherForecast] = useState()
+  const [weatherImg, setWeatherImg] = useState()
+  const [maxTemperature, setMaxTemperature] = useState()
+  const [minTemperature, setMinTemperature] = useState()
+  const [sunriseDate, setSunriseDate] = useState()
   const inputCity = useRef()
 
   async function weatherSearch() {
     const city = inputCity.current.value
 
-    const data = await axios.post(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&APPID=66df7171bb94888f0408af69c132b4ab`)
+    const weatherRequest = await axios.post(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&lang=pt_br&units=metric&APPID=66df7171bb94888f0408af69c132b4ab`)
+    
+    const weatherData = weatherRequest.data
+    const img = `http://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`
 
-    setWeatherForecast(data)
+    setWeatherForecast(weatherData)
+    setWeatherImg(img)
+    
+    const maxTemp = Math.max(
+      weatherData.list[0].main.temp_max,
+      weatherData.list[1].main.temp_max,
+      weatherData.list[2].main.temp_max,
+      weatherData.list[3].main.temp_max,
+      weatherData.list[4].main.temp_max,
+      weatherData.list[5].main.temp_max,
+      weatherData.list[6].main.temp_max,
+      weatherData.list[7].main.temp_max
+    )
 
-    console.log(data)
+    const minTemp = Math.min(
+      weatherData.list[0].main.temp_min,
+      weatherData.list[1].main.temp_min,
+      weatherData.list[2].main.temp_min,
+      weatherData.list[3].main.temp_min,
+      weatherData.list[4].main.temp_min,
+      weatherData.list[5].main.temp_min,
+      weatherData.list[6].main.temp_min,
+      weatherData.list[7].main.temp_min
+    )
+
+    setMaxTemperature(maxTemp)
+    setMinTemperature(minTemp)
+
+    const tempo = weatherData.city.sunrise
+    const date = new Date(tempo);
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    const printDate = `${hour}h${minute}min`
+    setSunriseDate(printDate)
   }
 
+  
+  
   return (
     <div className="App">
-      <h1>Weather Forecast API</h1>
+      <h1>Previsão do Tempo</h1>
       <div>
-        <input ref={inputCity} placeholder="Qual cidade você deseja a previsão do tempo?"></input>
+        <input ref={inputCity} placeholder="Insira a cidade desejada"></input>
         <button onClick={weatherSearch}>Pesquisar</button>
       </div>
-      { weatherForecast ? (
+      {weatherForecast ? (
         <div>
+          <img src={weatherImg} alt="icone-do-clima-atual" />
           <ul>
-            <li>{weatherForecast.data.name}, {weatherForecast.data.sys.country}</li>
+            <li>{weatherForecast.city.name}, {weatherForecast.city.country}</li>
+            <li>.</li>
             <li>Data atual completa</li>
-            <li>{weatherForecast.data.main.temp} graus Celsius</li>
-            <li>Temperatura Máxima</li>
-            <li>Temperatura Mínima</li>
-            <li>Condições Atuais:</li>
-            <li>Umidade {weatherForecast.data.main.humidity}%</li>
-            <li>Pressão Atmosférica {weatherForecast.data.main.pressure} hPa</li>
-            <li>Nascer do Sol</li>
+            <li>.</li>
+            <li>Temperatura Atual: {weatherForecast.list[0].main.temp} ºC</li>
+            <li>Sensação Térmica: {weatherForecast.list[0].main.feels_like} ºC</li>
+            <li>.</li>
+            <li>Condições Atuais: {weatherForecast.list[0].weather[0].description}</li>
+            <li>Nuvens no Céu: {weatherForecast.list[0].clouds.all}%</li>
+            <li>Umidade {weatherForecast.list[0].main.humidity}%</li>
+            <li>Pressão Atmosférica {weatherForecast.list[0].main.pressure} hPa</li>
+            <li>.</li>
+            <li>Temperatura Máxima para as próximas 24 horas: {maxTemperature}ºC</li>
+            <li>Temperatura Mínima para as próximas 24 horas: {minTemperature}ºC</li>
+            <li>.</li>
+            <li>Nascer do Sol: {sunriseDate}</li>
             <li>Pôr do Sol</li>
           </ul>
         </div>
       ) : null}
-      </div>
-      );
+    </div>
+  );
 }
 
-      export default App;
+export default App;
